7768698afb5798d5b13d2d175a36c186c2d2d449
morand-g <gaetan.morand@outlook.com>
2022-03-30 09:12:31 +0400

    modified:   docs/source/conf.py 	modified:   setup.cfg

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('/media/Data/Code/geoenrich/'))
 # -- Project information -----------------------------------------------------
 
 project = 'geoenrich'
-copyright = '2022, Gaétan Morand'
-author = 'Gaétan Morand'
+copyright = '2022, Gaétan Morand (UMR Marbec)'
+author = 'Gaétan Morand (UMR Marbec)'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
@@ -30,7 +30,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,9 +46,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
