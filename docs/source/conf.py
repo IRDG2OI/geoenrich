618c0908f172e6169d32248d9e67c9f94bac6be5
morand-g <gaetan.morand@outlook.com>
2022-03-30 09:21:35 +0400

    modified:   source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/media/Data/Code/geoenrich/'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
