b603fdb66a2de2f4dc5379c97e85df27c6c2491c
morand-g <gaetan.morand@outlook.com>
2022-03-30 09:24:43 +0400

    modified:   source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../geoenrich'))
+sys.path.insert(0, os.path.abspath('../geoenrich'))
 
 
 # -- Project information -----------------------------------------------------
