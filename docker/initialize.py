0d8e84c413bb7fdba2fd1df859464aeaa26d865a
morand-g <gaetan.morand@outlook.com>
2022-08-04 16:33:23 +0400

    modified:   docker/Dockerfile 	modified:   docker/docker-compose.yml 	modified:   docker/initialize.py

--- docker/initialize.py
+++ docker/initialize.py
@@ -1,6 +1,11 @@
 from pathlib import Path 
 import requests
 
+app_path = Path('/app')
+
+# Download files from github if they don't already exist
+
+
 # if not(Path('app/static/styles/style.css').exists()):
 # 	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/static/styles/style.css')
 # 	Path('app/static/styles/').mkdir(parents=True)
