d3207b9940d50ff8fd3f5d61d08a1f5f3f3dff1d
morand-g <gaetan.morand@outlook.com>
2022-08-05 11:18:43 +0400

    modified:   initialize.py

--- docker/initialize.py
+++ docker/initialize.py
@@ -2,25 +2,24 @@ from pathlib import Path
 import requests
 
 print('Initialization...')
-print(len(list(Path('/app').iterdir())))
 
-if not(len(list(Path('/app').iterdir()))):
+if not any(Path('/app').iterdir()):
 
-	# Download files from github if they don't already exist
-	Path('/app/data/').mkdir()
-	Path('/app/templates/').mkdir()
-	Path('/app/static/styles/').mkdir(parents=True)
-	
- 	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/static/styles/style.css')	
-	Path('/app/static/styles/style.css').open('wb').write(r.content)
+    # Download files from github if they don't already exist
+    Path('/app/data/').mkdir()
+    Path('/app/templates/').mkdir()
+    Path('/app/static/styles/').mkdir(parents=True)
+    
+    r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/static/styles/style.css') 
+    Path('/app/static/styles/style.css').open('wb').write(r.content)
 
-	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/templates/home.html')	
-	Path('/app/templates/home.html').open('wb').write(r.content)
+    r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/templates/home.html') 
+    Path('/app/templates/home.html').open('wb').write(r.content)
 
-	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/templates/download.html')	
-	Path('/app/templates/download.html').open('wb').write(r.content)
+    r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/templates/download.html') 
+    Path('/app/templates/download.html').open('wb').write(r.content)
 
-	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/main.py')	
-	Path('/app/main.py').open('wb').write(r.content)
+    r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/main.py') 
+    Path('/app/main.py').open('wb').write(r.content)
 
-	print('Initialization complete.')
+    print('Initialization complete.')
