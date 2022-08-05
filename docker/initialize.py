9d03200022abc399800a93f9718f52863809ce3d
morand-g <gaetan.morand@outlook.com>
2022-08-05 11:04:21 +0400

    modified:   docker-compose.yml 	modified:   initialize.py

--- docker/initialize.py
+++ docker/initialize.py
@@ -10,13 +10,13 @@ if not any(Path('/app').iterdir()):
 	Path('/app/static/styles/').mkdir(parents=True)
 	
  	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/static/styles/style.css')	
-	Path('app/static/styles/style.css').open('wb').write(r.content)
+	Path('/app/static/styles/style.css').open('wb').write(r.content)
 
 	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/templates/home.html')	
-	Path('app/templates/home.html').open('wb').write(r.content)
+	Path('/app/templates/home.html').open('wb').write(r.content)
 
 	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/templates/download.html')	
-	Path('app/templates/download.html').open('wb').write(r.content)
+	Path('/app/templates/download.html').open('wb').write(r.content)
 
 	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/main.py')	
-	Path('app/main.py').open('wb').write(r.content)
+	Path('/app/main.py').open('wb').write(r.content)
