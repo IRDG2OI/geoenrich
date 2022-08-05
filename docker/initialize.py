d16bf625a01a949f222294571b0f4889528f86ab
morand-g <gaetan.morand@outlook.com>
2022-08-05 11:01:56 +0400

    modified:   Dockerfile 	modified:   docker-compose.yml 	modified:   initialize.py

--- docker/initialize.py
+++ docker/initialize.py
@@ -1,13 +1,22 @@
 from pathlib import Path 
 import requests
 
-app_path = Path('/app')
 
-# Download files from github if they don't already exist
+if not any(Path('/app').iterdir()):
 
+	# Download files from github if they don't already exist
+	Path('/app/data/').mkdir()
+	Path('/app/templates/').mkdir()
+	Path('/app/static/styles/').mkdir(parents=True)
+	
+ 	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/static/styles/style.css')	
+	Path('app/static/styles/style.css').open('wb').write(r.content)
 
-# if not(Path('app/static/styles/style.css').exists()):
-# 	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/static/styles/style.css')
-# 	Path('app/static/styles/').mkdir(parents=True)
-# 	Path('app/static/styles/style.css').open('wb').write(r.content)
-# 	Path('app/static/styles/style.css').open('wb').close()
+	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/templates/home.html')	
+	Path('app/templates/home.html').open('wb').write(r.content)
+
+	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/templates/download.html')	
+	Path('app/templates/download.html').open('wb').write(r.content)
+
+	r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/develop/docker/app/main.py')	
+	Path('app/main.py').open('wb').write(r.content)
