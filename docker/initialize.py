74d7f6ca4f72901179b9c72780c57d93fac9294b
morand-g <gaetan.morand@outlook.com>
2023-01-27 15:47:26 +0400

    new file:   favicon.ico 	new file:   logo_france.png 	new file:   logo_g2oi.png 	new file:   logo_github.png 	new file:   logo_ird.png 	new file:   logo_reunion.png 	new file:   logo_rtd.png 	new file:   logo_ue.png 	modified:   ../../templates/download.html 	modified:   ../../templates/home.html 	modified:   ../../../initialize.py

--- docker/initialize.py
+++ docker/initialize.py
@@ -24,6 +24,9 @@ if not(Path('/app/static/uploads/').exists()):
 if not(Path('/app/static/stats/').exists()):   
     Path('/app/static/stats/').mkdir(parents=True)
 
+if not(Path('/app/static/stats/').exists()):   
+    Path('/app/static/assets/').mkdir(parents=True)
+
 if not(Path('/app/static/styles/style.css').exists()): 
     r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/main/docker/app/static/styles/style.css') 
     Path('/app/static/styles/style.css').open('wb').write(r.content)
@@ -36,9 +39,18 @@ if not(Path('/app/templates/download.html').exists()):
     r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/main/docker/app/templates/download.html') 
     Path('/app/templates/download.html').open('wb').write(r.content)
 
-if not(Path('/app/main.py').exists()): 
-    r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/main/docker/app/main.py') 
-    Path('/app/main.py').open('wb').write(r.content)
+if not(Path('/app/templates/download.html').exists()): 
+    r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/main/docker/app/templates/download.html') 
+    Path('/app/templates/download.html').open('wb').write(r.content)
+
+
+assets = ['favicon.ico', 'logo_france.png', 'logo_g2oi.png', 'logo_github.png', 'logo_ird.png',
+          'logo_reunion.png', 'logo_rtd.png', 'logo_ue.png']
+
+for asset in assets:
+    if not(Path('/app/static/assets/' + asset).exists()): 
+        r = requests.get('https://raw.githubusercontent.com/morand-g/geoenrich/main/docker/app/static/assets/' + asset) 
+        Path('/app/static/assets/' + asset).open('wb').write(r.content)
 
 if Path('/app/conf/credentials.py').exists():
     shutil.copy(Path('/app/conf/credentials.py'), Path('/usr/local/lib/python3.8/site-packages/geoenrich/credentials.py'))
